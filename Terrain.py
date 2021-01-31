0e0270571a27c0e4f9504b222a3f50740ba4bc7d
alterGNU <alter.gnu@gmail.com>
2021-01-31 20:20:09 +0100

    LG: Add Terrain.grille()

--- Terrain.py
+++ Terrain.py
@@ -1,4 +1,10 @@
 class Terrain(object):
-    def __init__(self, nblignes, nbcolonnes):
-        self.nblignes = nblignes
-        self.nbcolonnes = nbcolonnes
+    def __init__(self, nbLignes, nbColonnes):
+        self.nbLignes = nbLignes
+        self.nbColonnes = nbColonnes
+
+    def grille(self):
+        """
+        Retourne la grille du terrain, cad un tableau de tableau (remplis de None=vide)
+        """
+        return [[None] * self.nbColonnes] * self.nbLignes
