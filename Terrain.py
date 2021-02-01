a47f331edfae1283feffe8ff8e8ec393c27d954c
unknown <lanzark43@gmail.com>
2021-02-01 11:11:50 +0100

    ajout des methodes ajout_objet et ajout_alea

--- Terrain.py
+++ Terrain.py
@@ -1,3 +1,4 @@
+import random 
 class Terrain(object):
     def __init__(self, nbLignes, nbColonnes):
         self.nbLignes = nbLignes
@@ -8,7 +9,8 @@ class Terrain(object):
         Retourne la grille du terrain, cad un tableau de tableau (remplis de None=vide)
         """
         return [[None] * self.nbColonnes] * self.nbLignes
-
+    
+    
     def casevide(self, x, y, grille):
         """ int * int * tab [][] -> boolean
         retourne vrai si la case est vide, et faux si celle-ci est occupee"""
@@ -18,3 +20,30 @@ class Terrain(object):
         if grille[x][y] is None:
             return True
         return False
+    
+    @staticmethod
+    def alea(max):
+        """ int -> int 
+        rend un entier aleatoire compris entre 0 et max."""
+        return random.randint(0, max)  
+
+    def ajout_objet(self,grille,item,x,y) : 
+        """tab[][] * Object * int *int -> void
+        Place un objet donné en argument dans la case[x][y] du terrain en verifiant s'il est vide."""
+        if(self.casevide(x,y,grille)):
+            grille[x][y]==item
+        else : 
+            print("cette case est occupé.") 
+    
+    def ajout_alea(self,grille,item,nbitem):
+        """tab[][] * Object * int -> void
+        Place item nbfois aleatoirement sur le Terrain."""
+        a=Terrain.alea(self.nbLignes)
+        b=Terrain.alea(self.nbColonnes)
+        x=0
+        while(x<nbitem):
+            while(self.casevide(a,b,grille)==False) : 
+                a=Terrain.alea(self.nbLignes)
+                b=Terrain.alea(self.nbColonnes)
+            self.ajout_objet(grille,item,a,b)
+            x=x+1
