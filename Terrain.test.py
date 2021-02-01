9c0432f5e6b59398e9041e35c14cbc68b3449e36
ivan-fr <ivan.besevic_fr@yahoo.com>
2021-02-01 13:24:42 +0100

    IB: Add test for terrain constructor

--- Terrain.test.py
+++ Terrain.test.py
@@ -1,6 +1,19 @@
 import random
 import Terrain
 
+def isGrille(grille):
+    lineLength = len(grille[0])
+    return all(lineLength == len(line) for line in grille)
+
+def testConstructTerrain():
+    random_ligne = random.randint(1, 2000)
+    random_colonne = random.randint(1, 2000)
+    t = Terrain.Terrain(random_ligne, random_colonne)
+
+    assert t.nbLignes == random_ligne
+    assert t.nbColonnes == random_colonne
+    assert isGrille(t.grille)
+
 def testGrille():
     for _ in range(100):
         random_ligne = random.randint(1, 2000)
@@ -21,7 +34,13 @@ def testGrille():
 
 if __name__ == '__main__':
     try:
+        testConstructTerrain()
+        print("Test: Constructeur du terrain réussi")
+    except AssertionError as e:
+        print("Test: Constructeur du terrain a échoué !!")
+
+    try:
         testGrille()
         print("Test: Generation des grilles réussi")
     except AssertionError as e:
-        print("Test: Generation des grilles fails !!")
+        print("Test: Generation des grilles a échoué !!")
