15c451e3c9e0d90a134e43d109eabbc23ef9b90e
julie <julie.gaydon@gmail.com>
2021-02-01 10:45:51 +0100

    methode estvide dans classe Terrain

--- Terrain.py
+++ Terrain.py
@@ -8,3 +8,13 @@ class Terrain(object):
         Retourne la grille du terrain, cad un tableau de tableau (remplis de None=vide)
         """
         return [[None] * self.nbColonnes] * self.nbLignes
+
+    def casevide(self, x, y, grille):
+        """ int * int * tab [][] -> boolean
+        retourne vrai si la case est vide, et faux si celle-ci est occupee"""
+        if x > self.nbLignes or y > self.nbColonnes:
+            print("cette case n'est pas dans le tableau")
+            return False
+        if grille[x][y] is None:
+            return True
+        return False
