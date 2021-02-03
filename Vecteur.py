7931d9bd75ffccc0704d302f09d792bbe7116582
alterGNU <alter.gnu@gmail.com>
2021-02-03 19:00:35 +0100

    LG: Add method Vecteur.__str__()

--- Vecteur.py
+++ Vecteur.py
@@ -1,3 +1,5 @@
+import math
+
 class Vecteur(object):
     """
     Vecteur du plan : 2 dimensions
@@ -6,3 +8,10 @@ class Vecteur(object):
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+    def __str__(self):
+        """
+        -> str
+        Permet de retourner une chaine de caractère
+        """
+        return "("+str(self.x)+","+str(self.y)+")"
