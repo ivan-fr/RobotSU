d8b4e0abb96d2cb1aeade1ec02d26d7ee48c6145
Ivan BESEVIC <ivan.besevic_fr@yahoo.com>
2021-02-07 20:59:32 +0100

    IB: add test rotation Robot

--- Robot.test.py
+++ Robot.test.py
@@ -1,6 +1,7 @@
 import random
 import Robot
 import math
+
 def testConstructRobot():
 	random_x=random.randint(0, 50)
 	random_y=random.randint(0, 50)
@@ -12,6 +13,25 @@ def testConstructRobot():
 	assert r.y == random_y
 	assert r.vitesse == random_vitesse
 	assert r.angle == random_angle 
+
+def testRotation():
+	r = Robot.Robot(0., 0., 1., 0.)
+	oldAngle = r.angle
+	oldVecteurD = r.vecteurDeplacement
+	randomAngleRelative = random.uniform(-180, 180)
+	r.rotation(randomAngleRelative)
+
+	oldAngle += randomAngleRelative
+	oldAngle %= 360
+
+	if oldAngle > 180.:
+		oldAngle -= 360
+
+	assert abs(oldAngle - r.angle) < 0.0001
+	
+	compare_vecteur = r.vecteurDeplacement + oldVecteurD.rotation(randomAngleRelative) * (-1)
+	assert abs(compare_vecteur.norme()) < 0.00001
+
 	
 if __name__ == '__main__':
 	try:
@@ -19,3 +39,9 @@ if __name__ == '__main__':
 		print("Test: Constructeur de Robot réussi")
 	except AssertionError as e:
 		print("Test: Constructeur de Robot a échoué !!")
+
+	try:
+		testRotation()
+		print("Test: rotation de Robot réussi")
+	except AssertionError as e:
+		print("Test: rotation de Robot a échoué !!")
