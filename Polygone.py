f94bf6790c0bf520b719e9717a2713eb582372a1
ju <divmails@free.fr>
2021-02-11 18:36:53 +0100

    correction construct poly

--- Polygone.py
+++ Polygone.py
@@ -1,5 +1,4 @@
 import Vecteur
-import math
 
 class Polygone :
 
@@ -9,9 +8,9 @@ class Polygone :
         for i in range(len(liste_sommet)-1) :
             x1,y1 = liste_sommet[i]
             x2,y2 = liste_sommet[i+1]
-            v = Vecteur.Vecteur(abs(x1-x2),abs(y1-y2))
+            v = Vecteur.Vecteur((x1-x2),(y1-y2))
             self.liste_vecteur.append(v)
-        x0,y0 = liste_sommet[0]
-        xn,yn = liste_sommet[len(liste_sommet)-1]
-        v = Vecteur.Vecteur(abs(x0-xn),abs(y0-yn))
+        x0,y0 = liste_sommet[len(liste_sommet)-1]
+        xn,yn = liste_sommet[0]
+        v = Vecteur.Vecteur((x0-xn),(y0-yn))
         self.liste_vecteur.append(v)
