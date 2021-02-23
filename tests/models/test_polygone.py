34ab342adbfc8db44f6d08648d526d76786fb89d
Ivan BESEVIC <ivan.besevic_fr@yahoo.com>
2021-02-23 19:09:10 +0100

    IB: test generateur polygone

--- tests/models/test_polygone.py
+++ tests/models/test_polygone.py
@@ -2,21 +2,26 @@ import unittest
 import random
 from models import Polygone, Vecteur
 
+
 class PolygoneTest(unittest.TestCase):
+    def is_polygone(self, polygone):
+        v = Vecteur.Vecteur(0., 0.)
+
+        for vp in polygone.liste_vecteur:
+            v = v + vp
+
+        self.assertTrue(abs(v.x) <= 0.00001 and abs(v.y) <= 0.00001)
+
     def test_construct_polygone(self):
         liste_sommets = []
 
         for _ in range(20):
-            liste_sommets.append((random.uniform(10.,50.), random.uniform(10.,50.)))
+            liste_sommets.append(
+                (random.uniform(10., 50.), random.uniform(10., 50.)))
 
         p = Polygone.Polygone(liste_sommets)
 
-        v = Vecteur.Vecteur(0.,0.)
-
-        for vp in p.liste_vecteur:
-            v = v + vp
-
-        self.assertTrue(abs(v.x) <= 0.00001 and abs(v.y) <= 0.00001)
+        self.is_polygone(p)
 
     def test_collision(self):
         """tuple(int * int) *  Vecteur -> boolean
@@ -26,11 +31,13 @@ class PolygoneTest(unittest.TestCase):
         liste_sommets = []
 
         for _ in range(20):
-            liste_sommets.append((random.uniform(10.,50.), random.uniform(10.,50.)))
+            liste_sommets.append(
+                (random.uniform(10., 50.), random.uniform(10., 50.)))
 
         p = Polygone.Polygone(liste_sommets)
-        posRobot = (random.uniform(10.,50.), random.uniform(10.,50.))
-        vD = Vecteur.Vecteur(random.uniform(10.,50.), random.uniform(10.,50.))
+        posRobot = (random.uniform(10., 50.), random.uniform(10., 50.))
+        vD = Vecteur.Vecteur(random.uniform(10., 50.),
+                             random.uniform(10., 50.))
 
         b = False
         i = 0
@@ -39,4 +46,16 @@ class PolygoneTest(unittest.TestCase):
                 b = True
             i = i + 1
 
-        self.assertTrue(b == p.collision(posRobot, vD))    
+        self.assertTrue(b == p.collision(posRobot, vD))
+
+    def test_Carre(self):
+        self.is_polygone(Polygone.Carre(
+            (random.uniform(10, 100), random.uniform(10, 100)), random.uniform(10, 100)))
+
+    def test_Triangle(self):
+        self.is_polygone(Polygone.Triangle(
+            (random.uniform(10, 100), random.uniform(10, 100)), random.uniform(10, 100)))
+
+    def test_hexagone(self):
+        self.is_polygone(Polygone.hexagone(
+            (random.uniform(10, 100), random.uniform(10, 100)), random.uniform(10, 100)))
