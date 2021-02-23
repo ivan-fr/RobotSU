78331081c98195a2aa982076f3071b8f89c394ed
Ivan BESEVIC <ivan.besevic_fr@yahoo.com>
2021-02-23 18:55:56 +0100

    IB: ajout test terrain continue

--- tests/models/test_terrainContinu.py
+++ tests/models/test_terrainContinu.py
@@ -1,5 +1,16 @@
 import unittest
+from models import TerrainContinu, Robot, Polygone
 
 class TerrainContinuTest(unittest.TestCase):
     def test_contruct_tc(self):
-        pass
+        tc = TerrainContinu.Carre(20, (1.,1.))
+        self.assertIsNotNone(tc.vecteursSurface)
+        self.assertIsNotNone(tc.robot)
+        self.assertIsNotNone(tc.listePolygone)
+
+    def test_ajout_polygone(self):
+        tc = TerrainContinu.Carre(20, (1.,1.))
+        length = len(tc.listePolygone)
+        p = Polygone.Polygone(((0.,1.),(1.,0), (4,2)))
+        tc.ajoutPolygone(p)
+        self.assertTrue(length + 1 == len(tc.listePolygone))
