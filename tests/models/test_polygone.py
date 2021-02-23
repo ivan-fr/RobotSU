ffef2576dff199d49e29badfaa336ad97a90f4c8
Ivan BESEVIC <ivan.besevic_fr@yahoo.com>
2021-02-23 18:47:49 +0100

    IB: ajout test colision polygone

--- tests/models/test_polygone.py
+++ tests/models/test_polygone.py
@@ -17,3 +17,26 @@ class PolygoneTest(unittest.TestCase):
             v = v + vp
 
         self.assertTrue(abs(v.x) <= 0.00001 and abs(v.y) <= 0.00001)
+
+    def test_collision(self):
+        """tuple(int * int) *  Vecteur -> boolean
+        methode qui verifie la collision du robot avec un objet.
+        """
+
+        liste_sommets = []
+
+        for _ in range(20):
+            liste_sommets.append((random.uniform(10.,50.), random.uniform(10.,50.)))
+
+        p = Polygone.Polygone(liste_sommets)
+        posRobot = (random.uniform(10.,50.), random.uniform(10.,50.))
+        vD = Vecteur.Vecteur(random.uniform(10.,50.), random.uniform(10.,50.))
+
+        b = False
+        i = 0
+        while (i < len(p.liste_sommet)):
+            if p.liste_vecteur[i].collision(p.liste_sommet[i], vD, posRobot):
+                b = True
+            i = i + 1
+
+        self.assertTrue(b == p.collision(posRobot, vD))    
