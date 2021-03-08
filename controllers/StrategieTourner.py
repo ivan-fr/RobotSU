d75f4f72c962d3e986b95e0fef88d01e37e31885
ju <divmails@free.fr>
2021-03-08 12:03:43 +0100

    condition arret stategie tourner

--- controllers/StrategieTourner.py
+++ controllers/StrategieTourner.py
@@ -5,11 +5,14 @@ class StrategieTourner(object) :
     def __init__(self, rotationAngle, tc):
         self.tc = tc
         self.rotationAngle = rotationAngle
-        self.stop = False
+        self.ancienAngle = self.tc.angle
 
     def start(self):
-        self.stop = False
+        self.ancienAngle = self.tc.angle
 
     def step(self):
         self.tc.robot.rotation(self.rotationAngle)
-        self.stop = True
+
+    def stop(self):
+        # condition d'arret, si le robot a bien tourne de l'angle demande  
+        return self.tc.robot.angle >= self.ancienAngle+self.rotationAngle
