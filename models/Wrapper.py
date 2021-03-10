2785ec20ce2f02bdc65653a168a737c89f92f4db
RomainMarques <romainmarques01@gmail.com>
2021-03-10 12:02:55 +0100

    modification nom variable + amélioration de la rotation

--- models/Wrapper.py
+++ models/Wrapper.py
@@ -4,7 +4,7 @@ class Wrapper(object):
     def __init__(self, RobotIRL):
         self.RobotIRL = RobotIRL
         self._vitesse = 0
-        self._rotation = 0
+        self.degreParSeconde = 0
 
     @property
     def vitesse(self):
@@ -16,12 +16,19 @@ class Wrapper(object):
         self._vitesse = vitesse
 
     @property
-    def rotation(self):
-        return self._rotation
+    def degreParSeconde(self):
+        return self.degreParSeconde
 
-    @rotation.setter
-    def rotation(self, motor, angle):
+    @degreParSeconde.setter
+    def degreParSeconde(self, motor, angle):
         """passer en parametres pour motor: MOTOR_LEFT ou MOTOR_RIGHT en fonction de vers où tourner
         ainsi que l'angle"""
-        self.RobotIRL.set_motor_dps(motor, angle)
-        self._rotation = angle
+        if(motor == "MOTOR_LEFT"):
+            self.RobotIRL.set_motor_dps(motor, angle)
+            self.RobotIRL.set_motor_dps("MOTOR_RIGHT", -angle)
+            self._degreParSeconde = angle
+        elif(motor == "MOTOR_RIGHT"):
+            self.RobotIRL.set_motor_dps(motor, angle)
+            self.RobotIRL.set_motor_dps("MOTOR_LEFT", -angle)
+            self._degreParSeconde = angle
+        
