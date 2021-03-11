b78ef90bc0a7e2bb08bae90047739072c8918838
RomainMarques <romainmarques01@gmail.com>
2021-03-11 13:34:42 +0100

    fonction transmettant les ordres de la simu au robotIRL

--- models/Wrapper.py
+++ models/Wrapper.py
@@ -1,34 +1,35 @@
-from models import RobotIRL, Robot
+from models import RobotIRL, Robot, Vecteur
+import math
 
 class Wrapper(object):
     def __init__(self, RobotIRL):
         self.RobotIRL = RobotIRL
         self._vitesse = 0
-        self.degreParSeconde = 0
+        self._degreParSeconde = 0
+        self._vecteurDeplacement = None
+        self.bouger = 0
 
     @property
-    def vitesse(self):
-        return self._vitesse
+    def vecteurDeplacement(self):
+        return self._vecteurDeplacement
 
-    @vitesse.setter
-    def vitesse(self, vitesse):
-        self.RobotIRL.set_motor_dps("MOTOR_LEFT+MOTOR_RIGHT", vitesse)
-        self._vitesse = vitesse
+    @vecteurDeplacement.setter
+    def vecteurDeplacement(self, vecteurDeplacement):
+        self._vecteurDeplacement = vecteurDeplacement
+        self._degreParSeconde = self.fromVecteurToAngle(vecteurDeplacement)
+        self.bouger = self.mouvement()
+
+    def fromVecteurToAngle(self, vecteurDeplacement):
+        x = vecteurDeplacement.x
+        y = vecteurDeplacement.y
+        return 2*math.atan(y/(x+math.sqrt(x**2 + y**2)))
 
     @property
     def degreParSeconde(self):
         return self.degreParSeconde
 
-    @degreParSeconde.setter
-    def degreParSeconde(self, motor, angle):
+    def mouvement(self):
         """passer en parametres pour motor: MOTOR_LEFT ou MOTOR_RIGHT en fonction de vers où tourner
         ainsi que l'angle"""
-        if(motor == "MOTOR_LEFT"):
-            self.RobotIRL.set_motor_dps(motor, angle)
-            self.RobotIRL.set_motor_dps("MOTOR_RIGHT", -angle)
-            self._degreParSeconde = angle
-        elif(motor == "MOTOR_RIGHT"):
-            self.RobotIRL.set_motor_dps(motor, angle)
-            self.RobotIRL.set_motor_dps("MOTOR_LEFT", -angle)
-            self._degreParSeconde = angle
+            self.RobotIRL.set_motor_dps("MOTOR_RIGHT", self.degreParSeconde)
         
