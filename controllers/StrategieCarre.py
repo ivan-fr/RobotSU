f3f72ba1b5a7fc51381947bbb5066816232b2033
ju <divmails@free.fr>
2021-03-28 14:28:32 +0200

    Strategie carre avec liste

--- controllers/StrategieCarre.py
+++ controllers/StrategieCarre.py
@@ -5,31 +5,34 @@ class StrategieCarre(object):
         self.robot = robot
         self.stratAvancer = StrategieAvancerDroit.StrategieAvancerDroit(coteCarre, vitesse, robot, wrapper)
         self.stratTourner = StrategieTourner.StrategieTourner(90., 20., robot, wrapper)
+        self._liste_strategies = []
+        self._liste_strategies.append(stratAvancer)
+        self._liste_strategies.append(stratTourner)
         
         self.nbCoteParcouru = 0
 
     def start(self):
         self.nbCoteParcouru = 0
-        self.stratAvancer.start()
-        self.stratTourner.start()
+        self._liste_strategies[0].start()
+        self._liste_strategies[1].start()
 
     def step(self):
         if self.stop():
             return
 
-        if not self.stratAvancer.stop():
-            self.stratAvancer.step()
-        elif not self.stratTourner.stop():
-            self.stratTourner.step()
+        if not self._liste_strategies[0].stop():
+            self._liste_strategies[0].step()
+        elif not self._liste_strategies[1].stop():
+            self._liste_strategies[1].step()
         
-        if self.stratTourner.stop() and self.stratAvancer.stop():
+        if self._liste_strategies[1].stop() and self._liste_strategies[0].stop():
             self.nbCoteParcouru += 1
 
             if self.nbCoteParcouru == 4:
                 pass
             else:
-                self.stratAvancer.start()
-                self.stratTourner.start()
+                self._liste_strategies[0].start()
+                self._liste_strategies[1].start()
     
     def stop(self):
         #condition d'arret lorsque le robot a parcouru les 4 cotes
