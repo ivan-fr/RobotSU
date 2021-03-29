ad02e8fe734354614340bf1e3110d69e5397c6d7
Ivan BESEVIC <ivan.besevic_fr@yahoo.com>
2021-03-29 13:05:54 +0200

    fix

--- controllers/StrategieAvancerDroitIRL.py
+++ controllers/StrategieAvancerDroitIRL.py
@@ -35,7 +35,7 @@ class StrategieAvancerDroitIRL(object):
     def stop(self):
         # condition d'arret, lorsque que le robot a parcourut la distance souhaitee ou qu'il a rencontre un obstacle
         result = self.parcouruIRL >= self.distance or self.wrapper.RobotIRL.get_distance() < 20
-        if result:
-            self.wrapper.RobotIRL.stop()
+        if result:            
             self.wrapper.vitesse = 0.
+            self.wrapper.RobotIRL.stop()
         return result
