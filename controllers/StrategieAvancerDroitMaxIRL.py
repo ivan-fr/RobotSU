547e9c3d48e9114b1e32e5f0c7e118faf1d6914f
unknown <lanzark43@gmail.com>
2021-04-06 10:25:42 +0200

    strategieavancerdroitmaxmur added

---
+++ controllers/StrategieAvancerDroitMaxIRL.py
@@ -0,0 +1,44 @@
+import math
+from models import Robot
+import datetime
+
+
+class StrategieAvancerDroitIRL(object):
+    def __init__(self, wrapper,vitessemax):
+        self.vitessemax = 500
+        self.wrapper = wrapper
+
+        self.parcouruIRL = 0.
+
+    def acceleration(self,deltaT):
+        self.accel = (self.vitessemax - self.wrapper.vitesse)/deltaT
+
+    def start(self):
+        self.parcouruIRL = 0.
+        self.lastUpdate = None
+        self.wrapper.RobotIRL.stop()
+
+    def step(self):
+        if self.stop():
+            return
+            
+        if self.wrapper.vitesse < 10e-3:
+            self.wrapper.vitesse = self.vitesse
+
+        if self.lastUpdate is None:
+            self.lastUpdate = datetime.datetime.now()
+        else:
+            now = datetime.datetime.now()
+            deltaT = (now - self.lastUpdate).total_seconds()
+            self.lastUpdate = now
+            self.acceleration(deltaT)
+            self.wrapper.vitesse = self.wrapper.vitesse + ((self.accel * deltaT)/2)
+            self.parcouruIRL += self.wrapper.last_avancement
+
+    def stop(self):
+        # condition d'arret, lorsque que le robot a parcourut la distance souhaitee ou qu'il a rencontre un obstacle
+        result = self.parcouruIRL >= self.distance or self.wrapper.RobotIRL.get_distance() < 20
+        if result:            
+            self.wrapper.vitesse = 0.
+            self.wrapper.RobotIRL.stop()
+        return result
