18390b73e21a217fc72968180aa0c2ce7e1cae07
Ivan BESEVIC <ivan.besevic_fr@yahoo.com>
2021-04-12 11:47:40 +0200

    push 2

--- tmesolo.py
+++ tmesolo.py
@@ -4,6 +4,8 @@ from controllers import StrategieTournerIRL, StrategieAvancerDroitIRL
 import threading
 import time
 import datetime
+import math
+from models import TerrainContinu
 
 try:
     from robot2I013 import Robot2I013
@@ -35,7 +37,7 @@ def q1_1(wrapper):
     t2.start()
 
 def q1_2(wrapper):
-    startAvancer = StrategieAvancerDroitIRL(wrapper, 70., 15.)
+    startAvancer = StrategieAvancerDroitIRL.StrategieAvancerDroitIRL(wrapper, 70., 15.)
 
     fps = 60
     t2 = threading.Thread(target=updateStrats, args=(startAvancer, fps))
@@ -46,6 +48,7 @@ def q1_2(wrapper):
 
 class StrategieTriangleEq(object):
     def __init__(self, stratAvancer):
+        # 120 degrés pour tourner
         self.liste_strategies = [stratAvancer, StrategieTournerIRL.StrategieTournerIRL(stratAvancer.wrapper, 120., 30.)] * 3
         self.i_liste_strategies = 0
 
@@ -66,15 +69,129 @@ class StrategieTriangleEq(object):
             else:
                 self.liste_strategies[self.i_liste_strategies].start()
 
+    def stop(self):
+        #condition d'arret lorsque le robot a parcouru les 4 cotes
+        try:
+            _ = self.liste_strategies[self.i_liste_strategies]
+            return False
+        except IndexError:
+            return True
+
 def q2_1(wrapper):
     startAvancer = StrategieAvancerDroitIRL.StrategieAvancerDroitIRL(wrapper, 70., 15.)
-    stratBalise = StrategieTriangleEq(startAvancer, startTourner)
+    triangle = StrategieTriangleEq(startAvancer)
 
     fps = 60
 
-    t2 = threading.Thread(target=updateStrats, args=(stratBalise, fps))
+    t2 = threading.Thread(target=updateStrats, args=(triangle, fps))
     t2.start()
+
+
+class StrategiePolygoneReg(object):
+    def __init__(self, stratAvancer, n):
+        # 120 degrés pour tourner
+        target = (n - 2) * math.pi / n
+        dps = target * 0.3
+        self.liste_strategies = [stratAvancer, StrategieTournerIRL.StrategieTournerIRL(stratAvancer.wrapper, target, dps)] * n
+        self.i_liste_strategies = 0
+
+    def start(self):
+        self.i_liste_strategies = 0
+        self.liste_strategies[self.i_liste_strategies].start()
+
+    def step(self):
+        if self.stop():
+            return
+
+        if not self.liste_strategies[self.i_liste_strategies].stop():
+            self.liste_strategies[self.i_liste_strategies].step()
+        else:
+            self.i_liste_strategies += 1
+            if self.stop():
+                return
+            else:
+                self.liste_strategies[self.i_liste_strategies].start()
+
+    def stop(self):
+        #condition d'arret lorsque le robot a parcouru les 4 cotes
+        try:
+            _ = self.liste_strategies[self.i_liste_strategies]
+            return False
+        except IndexError:
+            return True
+
+def q2_2(wrapper):    
+    startAvancer = StrategieAvancerDroitIRL.StrategieAvancerDroitIRL(wrapper, 70., 15.)
+    polygone = StrategiePolygoneReg(startAvancer, 8)
+
+    fps = 60
+
+    t2 = threading.Thread(target=updateStrats, args=(polygone, fps))
+    t2.start()
+
+
+class Strategie5cmMur(object):
+    def __init__(self, wrapper):
+        self.wrapper = wrapper
+
+    def start(self):
+        self.wrapper.robotIRL.stop()
+
+    def step(self):
+        if self.stop():
+            return
+            
+        if self.wrapper.vitesse < 10e-3:
+            self.wrapper.vitesse = 10
+
+    def stop(self):
+        # condition d'arret, lorsque que le robot a parcourut la distance souhaitee ou qu'il a rencontre un obstacle
+        result = self.wrapper.robotIRL.get_distance() * 10e-1 < 5.
+        if result:            
+            self.wrapper.vitesse = 0.
+            self.wrapper.RobotIRL.stop()
+        return result
+
+class longerLeMur(object):
+    def __init__(self, wrapper):
+        self.strat5cm = Strategie5cmMur(wrapper)
+        self.wrapper = wrapper
+        self.tourner90 = StrategieTournerIRL.StrategieTournerIRL(wrapper, 90., 30.)
+        self.i = -1
+
+    def start(self):
+        self.strat5cm.start()
+        self.wrapper.robotIRL.stop()
+        self.tourner90.start()
+
+    def step(self):
+        if self.stop():
+            return
+
+        if not self.strat5cm.stop():
+            self.strat5cm.step()
+        elif not self.tourner90.stop():
+            self.tourner90.step()
+        else:
+            self.tourner90.start()
+            self.strat5cm.start()
+            self.i += 1
+
+    def stop(self):
+        return self.i == 4
+
+def q2_3(wrapper):    
+    longerMur = longerLeMur(wrapper)
+
+    fps = 60
+
+    t2 = threading.Thread(target=updateStrats, args=(longerMur, fps))
+    t2.start()
+
+
+def q3_1(wrapper):
+
 
 if __name__ == '__main__':
     # q1_1(Wrapper.Wrapper(Robot2I013()))
-    q1_2(Wrapper.Wrapper(Robot2I013()))
+    q2_3(Wrapper.Wrapper(Robot2I013()))
