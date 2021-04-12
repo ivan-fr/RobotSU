59a67245037fa95976a856a001fa44a3a94b3f33
RomainMarques <romainmarques01@gmail.com>
2021-04-12 18:55:26 +0200

    stratégie IA + simuIA pour tester

---
+++ controllers/StrategieIA.py
@@ -0,0 +1,34 @@
+from models import Robot
+from controllers import StrategieTournerIRL
+import time
+
+class StrategieIA(object):
+    def __init__(self, wrapper, vitessemax):
+        self.wrapper = wrapper
+        self.vitessemax = vitessemax
+        self.stopstrat = False
+        self.stratTourner = StrategieTournerIRL.StrategieTournerIRL(wrapper, 5., 5.)
+
+    def start(self):
+        self.wrapper.RobotIRL.stop()
+        self.stratTourner.start()
+
+    def step(self):
+        while not self.stop():
+            if self.wrapper.get_distance() < 0.5:
+                self.wrapper._vitesse = self.wrapper._vitesse // 2
+                time.sleep(0.1)
+                self.wrapper._vitesse = 0
+                angleTourne = 0.
+                while self.wrapper.get_ditance() < 0.5:
+                    if angleTourne >= 360:
+                        self.stopstrat = True
+                    self.stratTourner.step()
+                    angleTourne += 5
+            else:
+                self.wrapper._vitesse = self.vitessemax
+
+    def stop():
+        if self.stopstrat:
+            self.wrapper.RobotIRL.stop()
+        return self.stopstrat
---
+++ simuIA.py
@@ -0,0 +1,33 @@
+from views import Terrain
+from models import TerrainContinu, Robot, Polygone, Wrapper
+from controllers import StrategieIA
+import threading
+import time
+
+try:
+    from robot2I013 import Robot2I013
+except: 
+    from models.RobotIRLInterface import RobotIRLInterface as Robot2I013
+
+stop_thread = True
+
+def updateStrats(stratIA, fps):
+    stratIA.start()
+    while not stratIA.stop():
+        stratIA.step()
+        time.sleep(1./fps)
+    global stop_thread
+    stop_thread = False
+
+def run():
+    wrapper = Wrapper.Wrapper(Robot2I013())
+    stratIA = StrategieIA.StrategieIA(wrapper, 70.)
+
+    fps = 60
+
+    t2 = threading.Thread(target=updateStrats, args=(stratIA, fps))
+    t2.start()
+
+
+if __name__ == '__main__':
+    run()
